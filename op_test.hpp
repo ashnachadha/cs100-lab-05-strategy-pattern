09a048ed4c5faefd616403a029f5d344ef917fcf
ashnachadha <ashnachadha@gmail.com>
2020-02-13 14:30:36 -0800

    Add composite pattern lab files

---
+++ op_test.hpp
@@ -0,0 +1,23 @@
+#ifndef __OP_TEST_HPP__
+#define __OP_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "op.hpp"
+
+TEST(OpTest, OpEvaluateNonZero) {
+    Op* test = new Op(8);
+    EXPECT_EQ(test->evaluate(), 8);
+}
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->evaluate(), 0);
+}
+
+TEST(OpTest, OpEvaluateNegative) {
+    Op* test = new Op(-5);
+    EXPECT_EQ(test->evaluate(), -5);
+}
+
+#endif //__OP_TEST_HPP__
