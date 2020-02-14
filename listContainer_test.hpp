f8e713fa21f41ae92f72be8de0905de1f3d4e9a3
isis52300 <isis.dumas000@gmail.com>
2020-02-13 16:16:21 -0800

    Added tests to listContainer_test.hpp

--- listContainer_test.hpp
+++ listContainer_test.hpp
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 
 #include "op.hpp"
+#include "add.hpp"
 #include "listContainer.hpp"
 
 TEST(listContainerTest, AddOpToList) {
@@ -15,6 +16,38 @@ TEST(listContainerTest, AddOpToList) {
     EXPECT_EQ(list->at(0)->evaluate(), 0);
 }
 
+TEST(listContainerTest, AddOtherBasesToList) {
+    Op* three = new Op(3);
+    Add* add = new Add(three, three);
+    
+    ListContainer* list = new ListContainer();
+    list->add_element(add);
+    
+    ASSERT_EQ(list->size(), 1);
+    EXPECT_EQ(list->at(0)->evaluate(), 6);
+}
+
+TEST(listContainerTest, AddMultipleBasesToList) {
+    Op* three = new Op(3);
+    Add* add = new Add(three, three);
+    
+    ListContainer* list = new ListContainer();
+    list->add_element(three);
+    list->add_element(add);
+    
+    ASSERT_EQ(list->size(), 2);
+    EXPECT_EQ(list->at(0)->evaluate(), 3);
+    EXPECT_EQ(list->at(1)->evaluate(), 6);
+}
+
+TEST(listContainerTest, PrintOpFromList) {
+    Op* zero = new Op(0);
+    ListContainer* list = new ListContainer();
+    list->add_element(zero);
+    
+    ASSERT_EQ(list->size(), 1);
+    EXPECT_EQ(list->at(0)->print(), "0.000000\n");
+}
 
 #endif //__LISTCONTAINER_TEST_HPP__
 
